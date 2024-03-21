9f3964c45fd52ec528e58649114d061410277069
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-03-21 16:29:41 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from src.Chicken_disease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.Chicken_disease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.Chicken_disease import logger
-
+from src.Chicken_disease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
     data_ingestion = DataIngestionTrainingPipeline()  # If this line raises an error, provide more specific error handling here.
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(f"Error occurred in stage {STAGE_NAME}: {str(e)}")
+    raise e
+
+
+STAGE_NAME = "Prepare base model stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(f"Error occurred in stage {STAGE_NAME}: {str(e)}")
